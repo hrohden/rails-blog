496da974896ec760d4366fe774699e640ec21048
Henrique Broering Rohden <hrohden@gmail.com>
2024-02-19 15:53:27 -0300

    Add blog post titles to index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
 
+# This is a cool class
+class ApplicationController < ActionController::Base
   def index
+    @blog_post_titles = [
+      '10 Tips for Learning Python',
+      'Understanding JavaScript Closures',
+      "A Beginner's Guide to HTML and CSS",
+      'Exploring the New Features of React 17',
+      'How to Build a RESTful API with Node.js',
+      'Demystifying Machine Learning: A Comprehensive Guide',
+      'Getting Started with Docker: A Step-by-Step Tutorial',
+      'The Future of Web Development: Trends to Watch in 2022'
+    ]
   end
 end
