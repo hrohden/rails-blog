efdd2ad57c7e2ca4d03faa1422cf34cad0e6f32a
Henrique Broering Rohden <hrohden@gmail.com>
2024-02-27 11:37:17 -0300

    Refactor blog post retrieval in ApplicationController#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,6 @@
 # This is a cool class
 class ApplicationController < ActionController::Base
   def index
-    @blog_posts = [
-      '10 Tips for Learning Python',
-      'Understanding JavaScript Closures',
-      "A Beginner's Guide to HTML and CSS",
-      'Exploring the New Features of React 17',
-      'How to Build a RESTful API with Node.js',
-      'Demystifying Machine Learning: A Comprehensive Guide',
-      'Getting Started with Docker: A Step-by-Step Tutorial',
-      'The Future of Web Development: Trends to Watch in 2022'
-    ]
+    @blog_posts = BlogPost.all
   end
 end
