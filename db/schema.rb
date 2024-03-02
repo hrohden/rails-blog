96303059830d0f73b4b973e6d84ac87433ba5db1
Henrique Broering Rohden <hrohden@gmail.com>
2024-03-02 17:11:26 -0300

    Update schema.rb with new column 'content'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_200636) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_201007) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "headline"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_200636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.text "content"
   end
 
 end
