5b1d6288e8b18941fb8adf67de4edcda18e9fce7
Henrique Broering Rohden <hrohden@gmail.com>
2024-03-07 10:32:42 -0300

    Add comments association to Post model and display comments count in show.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_130034) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_132752) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -28,5 +36,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_130034) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "authors"
 end
