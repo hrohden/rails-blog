3937bc50415a660c9cfabd69ff37c658826a46bd
Henrique Broering Rohden <hrohden@gmail.com>
2024-03-09 16:09:35 -0300

    Add presence validation messages to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author
   has_many :comments
-  validates :title, :headline, :content, presence: true
+  validates :title, presence: { message: 'Please inform the post title' }
+  validates :headline, presence: { message: 'Headline cannot be blank' }
+  validates :content, presence: { message: 'Content cannot be blank' }
 end
