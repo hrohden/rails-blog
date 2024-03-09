37132372234a9a198731caef814128360b09df44
Henrique Broering Rohden <hrohden@gmail.com>
2024-03-09 13:29:22 -0300

    Add create action to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,20 @@ class PostsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :headline, :content)
+  end
 end
