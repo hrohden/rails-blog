7cea8ed74cdc9bdd4d6213fd4f06a8d811541ec5
Henrique Broering Rohden <hrohden@gmail.com>
2024-03-11 15:28:45 -0300

    Update comment variable name in posts controller and show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: { message: 'Please inform the post title' }
   validates :headline, presence: { message: 'Headline cannot be blank' }
   validates :content, presence: { message: 'Content cannot be blank' }
